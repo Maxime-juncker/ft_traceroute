1f4ffd4a5470a3157f0b37c64b3d169eeee569c2
Maxime Juncker <maxime.junck@gmail.com>
2025-09-03 14:43:11 +0200

    sending packet

--- srcs/main.c
+++ srcs/main.c
@@ -1,4 +1,5 @@
 #include "libft/memory.h"
+#include "libft/is.h"
 
 #include <errno.h>
 #include <sys/select.h>
@@ -9,7 +10,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
 
+#define ERROR(msg) dprintf(2, "ft_traceroute: %s: %s\n", msg, strerror(errno));
+
+typedef struct s_packet
+{
+	struct icmp		icmp;
+	struct iphdr	hdr;
+
+	char*	payload;
+
+} t_packet;
+
+typedef struct s_connection_info
+{
+	int sendfd;
+	int	recvfd;
+
+	struct sockaddr_in dest;
+} t_infos;
 
 struct addrinfo* getAddrIP(const char* name, char** ip)
 {
@@ -40,7 +61,119 @@ struct addrinfo* getAddrIP(const char* name, char** ip)
 	return res;
 }
 
-#define ERROR(msg) dprintf(2, "ft_traceroute: %s: %s\n", msg, strerror(errno));
+
+void show_packet(unsigned char* packet, size_t size)
+{
+	size_t	i;
+	char	buffer[17] = {0};
+	printf("+++ DUMP DATA +++\n");
+	for (i = 0; i < size; i++)
+	{
+		printf("%c%X", packet[i] < 16 ? '0' : '\0', packet[i]);
+		buffer[i % 16] = ft_isprint(packet[i]) ? packet[i] : '.';
+		if (i % 16 == 15)
+			printf("  %s\n", buffer);
+		else if (i % 2)
+			printf(" ");
+	}
+
+	while (i % 16 > 0)
+	{
+		printf("..");
+		if (i % 2)
+			printf(" ");
+		buffer[i % 16] = '.';
+		i++;
+		if (i % 16 == 0)
+			printf(" %s\n", buffer);
+	}
+}
+
+t_packet process_packet(char* packet, size_t size)
+{
+	struct iphdr*	hdr = (struct iphdr*)packet;
+	struct icmp*	icmp = (struct icmp*)(packet + sizeof(struct iphdr));
+	(void)icmp;
+	
+	show_packet((unsigned char*)packet, size);
+
+    // struct in_addr source, dest;
+
+    // source.s_addr = hdr->saddr;
+    // dest.s_addr = hdr->daddr;
+
+    // printf("Source IP: %s\n", inet_ntoa(source));
+    // printf("Destination IP: %s\n", inet_ntoa(dest));
+    //
+	return (t_packet) {
+		.icmp = *icmp,
+		.hdr = *hdr,
+		.payload = packet + sizeof(struct iphdr) + sizeof(struct icmp)
+	};
+}
+
+int send_packet(t_infos* infos, t_packet* packet)
+{
+	char send_buf[56]	= {0};
+	char recv_buf[128]	= {0};
+
+	if (sendto(infos->sendfd, send_buf, sizeof(send_buf), 0, (struct sockaddr*)&infos->dest, sizeof(infos->dest)) < 0)
+	{
+		ERROR("failed to send packet");
+		// cleanup(...);
+		return 1;
+	}
+
+	fd_set readfds;
+	struct timeval timeout = {3, 0};
+	FD_ZERO(&readfds);
+	FD_SET(infos->recvfd, &readfds);
+
+	int ready = select(infos->recvfd + 1, &readfds, NULL, NULL, &timeout);
+	if (ready == -1)
+	{
+		ERROR("select failed");
+		return 1;
+	}
+	if (ready == 0)
+	{
+		printf("timeout\n");
+		return 1; // change to continue
+	}
+
+	socklen_t len = sizeof(infos->dest);
+	recvfrom(infos->recvfd, recv_buf, sizeof(recv_buf), 0, (struct sockaddr*)&infos->dest, &len);
+	*packet = process_packet(recv_buf, sizeof(recv_buf));
+
+	return 0;
+}
+
+int find_path(t_infos* infos)
+{
+	t_packet packet;
+	ft_bzero(&packet, sizeof(packet));
+
+	int i = 0;
+	int ttl = 1;
+		infos->dest.sin_port = htons(33435 + i);
+	while (packet.icmp.icmp_type == ICMP_TIME_EXCEEDED || i == 0)	
+	{
+
+		if (send_packet(infos, &packet) != 0)
+		{
+			exit(0);
+		}
+
+		struct in_addr tmp;
+		tmp.s_addr = packet.hdr.daddr;
+		printf("%d: %s (ttl:%d)\n", i, inet_ntoa(tmp), packet.icmp.icmp_type);
+		ttl++;
+		i++;
+		sleep(1);
+	}
+	return 0;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -49,6 +182,7 @@ int main(int argc, char* argv[])
 		printf("missing args\n");
 		return 1;
 	}
+
 
 	char *ip;
 	getAddrIP(argv[1], &ip);
@@ -70,10 +204,16 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+	int ttl = 1;
+	if (setsockopt(sendfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(int)) != 0)
+	{
+		ERROR("failed to set ttl");
+		return 1;
+	}
+
 	struct sockaddr_in	dest;
 	ft_bzero(&dest, sizeof(struct sockaddr_in));
 	dest.sin_family = AF_INET;
-	dest.sin_port = htons(33434);
 	if (inet_aton(ip, &dest.sin_addr) == 0)
 	{
 		ERROR("invalid address");
@@ -82,29 +222,10 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	char send_buf[42];
-	if (sendto(sendfd, send_buf, sizeof(send_buf), 0, (struct sockaddr*)&dest, sizeof(dest)) < 0)
-	{
-		ERROR("failed to send packet");
-		// cleanup(...);
-		return 1;
-	}
+	t_infos infos;
+	infos.recvfd = recvfd;
+	infos.sendfd = sendfd;
+	infos.dest = dest;
+	find_path(&infos);
 
-	fd_set readfds;
-	struct timeval timeout = {3, 0};
-	FD_ZERO(&readfds);
-	FD_SET(recvfd, &readfds);
-
-	int ready = select(recvfd + 1, &readfds, NULL, NULL, &timeout);
-	if (ready == -1)
-	{
-		ERROR("select failed");
-		return 1;
-	}
-	if (ready == 0)
-	{
-		printf("timeout\n");
-		return 1; // change to continue
-	}
-	printf("%d\n", ready);
 }
